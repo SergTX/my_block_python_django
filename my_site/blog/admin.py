1bceb7f6734b0087256864af7783b515c9a99ab8
SergTX <Sergey2623rus@gmail.com>
2024-01-31 21:28:14 -0600

    Adding db, classes Author, Post, Tag and modifying admin menu

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Author, Post, Tag
+
+
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):                    # modifying Admin menu adding filters , slug-auto
+    list_filter = ("author", "tags", "date")          # adding filters by name in the tuple - will be available on ui side
+    list_display = ("title", "date", "author")        # display title , date , author columns
+    prepopulated_fields = {"slug": ("title",)}        # adding in dic what fields to populate and from what to take it
+
+
+admin.site.register(Post, PostAdmin)      # passing admin class as a second parameter
+admin.site.register(Author)
+admin.site.register(Tag)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):  # this func overrides what we want to return
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def full_name(self):  # adding a helper to return full name in admin ui
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):  # this func overrides what we want to return
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=80)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+
+
+# class TestTableBook(models.Model):
+#     title = models.CharField(max_length=50)
+#     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])    # set boundaries , validator
+#     author = models.CharField(null=True, max_length=30)
+#     is_bestSelling = models.BooleanField(default=False)
+#
+#     def __str__(self):   # if TestTableBook was requested in the console give me the output in this format
+#         return f'{self.title} ({self.rating})'
