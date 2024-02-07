9d3c5190a8a6eabdccf9db8c31f4efaf65244fc3
SergTX <Sergey2623rus@gmail.com>
2024-02-06 19:47:55 -0600

    wotking on single posts, displaying them, adding a new post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
+# from datetime import date
 from .models import Post
 
 
 # creating posts dummy for now
-all_posts = []
+# all_posts = []
 
 
-def get_date(post):
-    return post['date']
+# def get_date(post):
+#     return post['date']
 
 # Create your views here.
 
 
 def starting_page(request):  # request will pass dynamically by django
     latest_post = Post.objects.all().order_by("-date")[:3]    # query in desc order lasts posts by date,not support [-3:]
-    sorted_posts = sorted(all_posts, key=get_date)  # func get_date will go to every post and return date
+    # sorted_posts = sorted(all_posts, key=get_date)  # func get_date will go to every post and return date
     # latest_post = sorted_posts[-3:]   # getting the last 3 positions from the list of sorted dates
     return render(request, "blog/starting_page.html", {
         "posts": latest_post     # using posts varibale to display 3 latest post
@@ -23,15 +23,19 @@ def starting_page(request):  # request will pass dynamically by django
 
 
 def posts(request):         # for the post/ page
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
+        "all_posts": all_posts     # getting all post from the list
+        # now getting all posts from database
     })
 
 
 
 def single_detail(request, slug): # taking a slug , then we loop trough all posts and finding that post by provided slug, returning a post
     # where next() function finds a first element when condition met
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)  # if slug was not there it will return error
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
